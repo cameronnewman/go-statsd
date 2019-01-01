4a21e1c3b97d041a6fd2902a31697dfb07ff786a
Cameron Newman <cameron@newman.io>
2019-01-01 19:05:54 +1100

    Added example folder + added doc.go

--- doc.go
+++ doc.go
@@ -4,3 +4,28 @@ Package statsd is simple statsd warpper lib
 
 */
 package statsd
+
+// Simple example:
+//
+// package main
+//
+// import (
+// 	"fmt"
+// 	"github.com/cameronnewman/lib.statsd"
+// )
+//
+// func main() {
+//
+//	sd, err := statsd.New(&statsd.Config{
+//		StatsdHost: "127.0.0.1",
+//		StatsdPort: 9191,
+//		StatsdNamespace: "myapp"
+//	})
+//
+//	if err != nil{
+//		panic(err)
+//	}
+//
+//	sd.StatsCounter("Starting", 1)
+//
+// }
